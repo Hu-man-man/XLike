a56b591fa21cb2d5fc7dd2dc7ca7969ae97b51fc
Jason <jason.ricou@gmail.com>
2023-12-18 15:19:35 +0100

    correctif firebase config

--- app/page.js
+++ app/page.js
@@ -2,20 +2,20 @@
 
 import useFirebase from "./hooks/useFirebase"
 import  FirebaseContext  from "./hooks/context"
-import { firebaseConfig } from "./firebaseConfig"
+//import { firebaseConfig } from "./firebaseConfig"
 
 import Login from "./Login/page"
 import Feed from "./Feed/page"
 
 export default function Home() {
-//   const firebaseConfig = {
-//     apiKey: process.env.FIREBASE_API_KEY,
-//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//     appId: process.env.FIREBASE_APP_ID
-//   }
+  const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID
+  }
 
   const {user, firebase} = useFirebase(firebaseConfig);  //hook qui gère la connection
 
