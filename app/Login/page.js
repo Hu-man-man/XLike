de781bc09965eea6ee1eba555165c2c4cfb8a32c
Jason <jason.ricou@gmail.com>
2024-01-29 14:04:41 +0100

    versel12

--- app/Login/page.js
+++ app/Login/page.js
@@ -1,31 +1,45 @@
-"use client"
-import { useContext } from "react";
-import  FirebaseContext  from "../hooks/context"
+"use client";
+import { useContext, useState } from "react";
+import FirebaseContext from "../hooks/context";
 
 export default function Login() {
-    const { firebase} = useContext(FirebaseContext) || {};
-    const { auth } = firebase || {};
 
-    const handleLogin = () => {
-      if (auth && auth.login) {
-          auth.login();
-      } else {
-          console.error("Authentification non disponible");
-      }
-  };
+  const [loading, setLoading] = useState(true);
 
-    return (
-      <main>
-        <div className="flex items-center justify-center flex-col h-screen">
-          <h1 className="italic text-blue-600 font-black text-2xl font-['Comic_Sans_Ms']">Touiteur</h1>
-          <br />
-          <button
-            onClick={handleLogin}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Log In
-          </button>
-        </div>
-      </main>
-    );
-  }
+  const { firebase } = useContext(FirebaseContext) || {};
+  const { auth } = firebase || {};
+
+  const handleLogin = async () => {
+    if (!auth || !auth.login) {
+        console.error("Authentification non disponible");
+        return;
+    }
+
+    setLoading(true); // Mettre à jour l'état de chargement pour indiquer le chargement en cours
+
+    try {
+        await auth.login();
+    } catch (error) {
+        console.error("Erreur lors de la connexion :", error);
+    } finally {
+        setLoading(false); // Mettre à jour l'état de chargement une fois que la connexion est terminée (réussie ou non)
+    }
+};
+
+  return (
+    <main>
+      <div className="flex items-center justify-center flex-col h-screen">
+        <h1 className="italic text-blue-600 font-black text-2xl font-['Comic_Sans_Ms']">
+          Touiteur
+        </h1>
+        <br />
+        <button
+          onClick={handleLogin}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Log In
+        </button>
+      </div>
+    </main>
+  );
+}
