d2cc41ff40c98ae3cfb04f991821a08b4cef38ef
Jason <jason.ricou@gmail.com>
2024-01-29 14:18:35 +0100

    versel14

--- app/Login/page.js
+++ app/Login/page.js
@@ -35,9 +35,10 @@ export default function Login() {
         <br />
         <button
           onClick={handleLogin}
+          disabled={loading}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
-          Log In
+          {loading ? 'Chargement...' : 'Log In'}
         </button>
       </div>
     </main>
